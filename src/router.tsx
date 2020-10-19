2f764a232b8dfeabfe6f0d24a6ced59699ec586e
TiagoSeverino <tiago@severino.xyz>
2020-10-19 16:33:40 +0100

    Add custom header

--- src/router.tsx
+++ src/router.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import Header from './components/Header';
+
 import Map from './pages/Map';
 import MarkerDetails from './pages/MarkerDetails';
 
@@ -14,10 +16,20 @@ export default function Routes() {
 			<Navigator
 				screenOptions={{
 					headerShown: false,
+					cardStyle: {
+						backgroundColor: '#F2F3F5',
+					},
 				}}
 			>
 				<Screen name="Map" component={Map} />
-				<Screen name="MarkerDetails" component={MarkerDetails} />
+				<Screen
+					name="MarkerDetails"
+					component={MarkerDetails}
+					options={{
+						headerShown: true,
+						header: () => <Header title="Assistance Details" />,
+					}}
+				/>
 			</Navigator>
 		</NavigationContainer>
 	);
